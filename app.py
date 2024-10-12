d78c0d6fdf07aca98122d136abb708549548a2f1
Sugeng Riyanto <36408012+sugeng-riyanto@users.noreply.github.com>
2024-10-12 19:25:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,14 +9,12 @@ import streamlit.components.v1 as components
 def add_google_analytics(ga_id):
     analytics_script = f"""
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-FXEFNWY86D"></script>
 <script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-FXEFNWY86D');
-</script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){{dataLayer.push(arguments);}}
+      gtag('js', new Date());
+      gtag('config', '{ga_id}');
+    </script>
     """
     components.html(analytics_script, height=0)
 # Function to generate 2D sample data and return as a DataFrame
